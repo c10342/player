d9540dcbf6758bf2439f496c016d020c45d5ffc7
c10342 <1376023740@qq.com>
2026-04-17 20:38:05 +0800

    chore(*): vlc重构

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <img alt="logo" class="logo" src="./assets/electron.svg" />
+  <div class="creator">Powered by electron-vite</div>
+  <div class="text">
+    Build an Electron app with
+    <span class="vue">Vue</span>
+    and
+    <span class="ts">TypeScript</span>
+  </div>
+  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <div class="actions">
+    <div class="action">
+      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div>
+  </div>
+  <Versions />
+</template>
